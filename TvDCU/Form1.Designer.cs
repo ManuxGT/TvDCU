0a92d2c690ab7c7da74051a3d9b21cac754ae49d
ManuxGT <imccs05@hotmail.com>
2019-03-28 00:30:22 -0400

    Solo falta ponerle el label que diga en que canal estoy

--- TvDCU/Form1.Designer.cs
+++ TvDCU/Form1.Designer.cs
@@ -28,13 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.RangeBar = new System.Windows.Forms.TrackBar();
+            this.Player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.RangeBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RangeBar
+            // 
+            this.RangeBar.AccessibleDescription = "RangeBar";
+            this.RangeBar.AccessibleName = "RangeBar";
+            this.RangeBar.Location = new System.Drawing.Point(721, 296);
+            this.RangeBar.Maximum = 100;
+            this.RangeBar.Name = "RangeBar";
+            this.RangeBar.Size = new System.Drawing.Size(123, 45);
+            this.RangeBar.TabIndex = 12;
+            this.RangeBar.TabStop = false;
+            this.RangeBar.TickFrequency = 10;
+            this.RangeBar.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.RangeBar.Value = 50;
+            this.RangeBar.Scroll += new System.EventHandler(this.Scrolleado);
+            this.RangeBar.ValueChanged += new System.EventHandler(this.ValorCambiado);
+            // 
+            // Player
+            // 
+            this.Player.Enabled = true;
+            this.Player.Location = new System.Drawing.Point(3, 0);
+            this.Player.Name = "Player";
+            this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
+            this.Player.Size = new System.Drawing.Size(640, 452);
+            this.Player.TabIndex = 0;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::TvDCU.Properties.Resources.speaker_pere;
+            this.pictureBox6.Location = new System.Drawing.Point(667, 296);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(48, 30);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 13;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::TvDCU.Properties.Resources.VolumeDown;
+            this.pictureBox3.Location = new System.Drawing.Point(763, 202);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TvDCU.Properties.Resources.VolumeUP;
+            this.pictureBox2.Location = new System.Drawing.Point(763, 131);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TvDCU.Properties.Resources.ChannelDown;
+            this.pictureBox1.Location = new System.Drawing.Point(690, 202);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::TvDCU.Properties.Resources.ChannelUp;
+            this.pictureBox8.Location = new System.Drawing.Point(690, 131);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::TvDCU.Properties.Resources.mute;
+            this.pictureBox7.Location = new System.Drawing.Point(794, 24);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::TvDCU.Properties.Resources.Turn;
+            this.pictureBox5.Location = new System.Drawing.Point(673, 30);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(856, 450);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.RangeBar);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.Player);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.RangeBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Player;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        public System.Windows.Forms.TrackBar RangeBar;
     }
 }
 
--- TvDCU/Form1.cs
+++ TvDCU/Form1.cs
@@ -12,9 +12,109 @@ namespace TvDCU
 {
     public partial class Form1 : Form
     {
+        public List<string> CanalesURL = new List<string>();
+        public List<string> Canales = new List<string>();
+        public int Canal = 0;
+        public bool state = false;
         public Form1()
         {
             InitializeComponent();
+            InicializarCanales();
+    
+        }
+
+       
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (Canal!=0)
+            {
+                Canal--;
+                Player.URL = CanalesURL[Canal];
+            }
+            else if (Canal==0)
+            {
+                Canal = CanalesURL.Count - 1;
+                Player.URL = CanalesURL[Canal];
+            }
+        }
+
+        public void InicializarCanales()
+        {
+            Canales.Add("Real Madrid TV");
+            CanalesURL.Add("http://rmtv24hweblive-lh.akamaihd.net/i/rmtv24hwebes_1@300661/master.m3u8");
+            Canales.Add("Paramount Channel");
+            CanalesURL.Add("http://paramount.live.flumotion.com/live/playlist.m3u8");
+            Canales.Add("TeleRD");
+            CanalesURL.Add(" http://k4.usastreams.com/Telesistemas/Telesistemas/playlist.m3u8");
+            Canales.Add("Unknown");
+            CanalesURL.Add("http://play.streaminghd.cl/luis251/luis251/playlist.m3u8");
+            Canales.Add("Color visión");
+            CanalesURL.Add("http://ss2.domint.net:2138/cvh_str/colorvisionhd/playlist.m3u8");
+            Canales.Add("Fox Cinema");
+            CanalesURL.Add("http://161.0.157.7/PLTV/88888888/224/3221226793/03.m3u8");
+            Canales.Add("Teleformula");
+            CanalesURL.Add("http://wms30.tecnoxia.com/radiof/abr_radioftele/playlist.m3u8");
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (state)
+            {
+                Player.Ctlcontrols.stop();
+            }
+            else
+            {
+                Player.URL =CanalesURL[Canal];
+                Player.Ctlcontrols.play();
+            }
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            if (Canal+1!=CanalesURL.Count)
+            {
+                Canal++;
+                Player.URL = CanalesURL[Canal];
+            }else if (Canal+1==CanalesURL.Count)
+            {
+                Canal = 0;
+                Player.URL = CanalesURL[Canal];
+            }
+        }
+
+        private void ValorCambiado(object sender, EventArgs e)
+        {
+    
+            Player.settings.volume = RangeBar.Value;
+        }
+
+        private void Scrolleado(object sender, EventArgs e)
+        {
+            
+            Player.settings.volume = RangeBar.Value;
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+            RangeBar.Value = 0;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (RangeBar.Value+5<=100)
+            {
+                RangeBar.Value += 5;
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (RangeBar.Value-5>=0)
+            {
+                RangeBar.Value -=5;
+            }
         }
     }
 }
